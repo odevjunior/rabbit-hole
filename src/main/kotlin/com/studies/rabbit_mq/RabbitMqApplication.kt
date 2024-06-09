defb5863817907f7f076d3b70c18cc3f8dc6550b
Jairo  Junior <jairojunior@MacBook-Pro-de-Jairo.local>
2024-06-08 21:16:16 -0300

    initial scaffolding

---
+++ src/main/kotlin/com/studies/rabbit_mq/RabbitMqApplication.kt
@@ -0,0 +1,11 @@
+package com.studies.rabbit_mq
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RabbitMqApplication
+
+fun main(args: Array<String>) {
+	runApplication<RabbitMqApplication>(*args)
+}
